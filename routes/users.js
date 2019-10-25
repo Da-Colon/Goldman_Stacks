884785e4b27646788a8e13dc1d4961493cc8fa69
Da-Colon <dcolonantonio@gmail.com>
2019-10-24 21:13:09 -0400

    bug fixing

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
-// const UserModel = require("../models/users");
+const UserModel = require("../models/users");
 
-// router.get("/login", async(req, res, next) => {
-//     res.render("template", {
-//         locals: {
-//             title: "Login",
-//             isLoggedIn: req.session.is_logged_in
+router.get("/login", async(req, res, next) => {
+    res.render("template", {
+        locals: {
+            title: "Login",
+            isLoggedIn: req.session.is_logged_in
 
-//         },
-//         partials: {
-//             partial: "partial-login"
-//         }
-//     });
-// });
+        },
+        partials: {
+            partial: "partial-login"
+        }
+    });
+});
 
 router.get("/signup", async(req, res, next) => {
     res.render("template", {
         locals: {
-            title: "Sign Up"
-                // isLoggedIn: req.session.is_logged_in
+            title: "Sign Up",
+            isLoggedIn: req.session.is_logged_in
         },
         partials: {
             partial: "partial-signup"
@@ -28,44 +28,44 @@ router.get("/signup", async(req, res, next) => {
     });
 });
 
-// router.post("/signup", async(req, res, next) => {
-//     const { first_name, last_name, email_address } = req.body;
+router.post("/signup", async(req, res, next) => {
+    const { first_name, last_name, email_address } = req.body;
 
-//     const salt = bcrypt.genSaltSync(10);
-//     const hash = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-//     const user = new UserModel(first_name, last_name, email_address, hash);
+    const user = new UserModel(first_name, last_name, email_address, hash);
 
-//     const addUser = await user.save()
-//     console.log("Was User Added? ", addUser.id);
-//     if (addUser) {
-//         res.status(200).redirect("/users/login");
-//     } else {
-//         res.status(500);
-//     }
-// });
+    const addUser = await user.save()
+    console.log("Was User Added? ", addUser.id);
+    if (addUser) {
+        res.status(200).redirect("/users/login");
+    } else {
+        res.status(500);
+    }
+});
 
-// router.post("/login", async(req, res, next) => {
-//     const { email_address, password } = req.body;
+router.post("/login", async(req, res, next) => {
+    const { email_address, password } = req.body;
 
-//     const user = new UserModel(null, null, email_address, password);
+    const user = new UserModel(null, null, email_address, password);
 
-//     const response = await user.login();
-//     console.log(response);
+    const response = await user.login();
+    console.log(response);
 
-//     if (!!response.isValid) {
-//         const { id, first_name, last_name } = response;
+    if (!!response.isValid) {
+        const { id, first_name, last_name } = response;
 
-//         req.session.is_logged_in = true;
-//         req.session.first_name = first_name;
-//         req.session.last_name = last_name;
-//         req.session.id = id;
+        req.session.is_logged_in = true;
+        req.session.first_name = first_name;
+        req.session.last_name = last_name;
+        req.session.id = id;
 
-//         res.status(200).redirect("/");
+        res.status(200).redirect("/");
 
-//     } else {
-//         res.status(401);
-//     }
-// });
+    } else {
+        res.status(401);
+    }
+});
 
 module.exports = router;
