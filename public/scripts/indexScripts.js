af67d57c16832a8f56a4b5d18cd2f3895623b772
Da-Colon <dcolonantonio@gmail.com>
2019-10-28 16:15:40 -0400

    added a function to limit number of chars in first and last name

--- public/scripts/indexScripts.js
+++ public/scripts/indexScripts.js
@@ -4,17 +4,32 @@ const signupRow = document.querySelector('#signup-row');
 const loginRow = document.querySelector('#login-row');
 
 loginInsteadButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('login-instead-button-pressed');
+    e.preventDefault();
+    console.log('login-instead-button-pressed');
 
-  signupRow.classList.add('hide-row');
-  loginRow.classList.remove('hide-row');
+    signupRow.classList.add('hide-row');
+    loginRow.classList.remove('hide-row');
 });
 
 signupInsteadButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('signup-instead-button-pressed');
+    e.preventDefault();
+    console.log('signup-instead-button-pressed');
 
-  signupRow.classList.remove('hide-row');
-  loginRow.classList.add('hide-row');
+    signupRow.classList.remove('hide-row');
+    loginRow.classList.add('hide-row');
 });
+
+const firstName = document.getElementById('first_name');
+const LastName = document.getElementById('last_name');
+
+
+firstName.oninput = function() {
+    if (this.value.length > 12) {
+        this.value = this.value.slice(0, 12);
+    }
+}
+lastName.oninput = function() {
+    if (this.value.length > 12) {
+        this.value = this.value.slice(0, 12);
+    }
+}
